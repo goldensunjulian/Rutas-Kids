482f8371c558a78cb7a963905397adf7c86c7939
Julian <trifuerzatristan@gmail.com>
2026-05-23 21:22:09 -0500

    Update: version final

---
+++ JS/appLog.js
@@ -0,0 +1,17 @@
+const inputClave = document.getElementById('contraseña');
+const modal = document.getElementById("modal");
+const formularioLogin = document.querySelector('form');
+
+formularioLogin.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+    const claveIngresada = inputClave.value;
+
+    if (claveIngresada === "Tristan2105") {
+        modal.classList.add("show");
+        setTimeout(() => {
+            window.location.replace("index.html"); 
+        }, 50000);
+    } else {
+        alert("Contraseña incorrecta.");
+    }
+});
